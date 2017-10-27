6e4c01735cff0b9ae02c771653e50fba2dd37d63
Laurent Lemire <lemire.laurent824@hotmail.fr>
2017-10-27 15:08:55 -0400

    first commit with All

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int pomme = 2;
+		int poire = 6;
+		
+		System.out.println("Il y a " + pomme + " pommes  et " + poire + " poires" );
+
+	}
+
+}
